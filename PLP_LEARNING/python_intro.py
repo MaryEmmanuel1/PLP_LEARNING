403f05809e67fee3e0090a3698f85abba9b3b4a6
Mary Emmanuel <emmamaryg124@gmail.com>
2024-06-17 14:05:39 +0100

    This is my first commit

---
+++ PLP_LEARNING/python_intro.py
@@ -0,0 +1,43 @@
+#Correct Identation
+#age = 18
+#if age >= 18:
+ #   print("You are an Adult")
+#elif age > 12:
+ #   print("You are a Teenager")
+#else:
+ #   print("You are a Child")
+
+
+
+
+#List Comprehension
+# squares = [x**2 for x in range(10)]
+# print(squares)
+
+#For Loop
+# for i in range(5):
+#     print(f"i is i")
+
+
+#while Loop
+# count = 0
+# while count < 5:
+#     print(f"count is {count}")
+#     count += 1
+
+# #Defining functions
+# def greet(name):
+#     return f"Hello, {name}!"
+# print(greet("Alice"))
+
+
+#Arguments and Return values
+def add (a,b):
+    return a + b
+result = add(5,3)
+print(result)
+
+
+#Lambda Function
+multiply = lambda x,y: x * y
+print(multiply(3,5))
